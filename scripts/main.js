a9c3fcf2e1ace99bcf2bba5a41680933d05b3ed9
Dimm005 <dimm005@gmail.com>
2023-11-19 20:38:13 +0800

    Fix a bug in numToArr function
    Fix a bug when function returns "." as the last element of array.
    It happend if an argument has decimal point and more digits then
    limit
    
    Also and global variable for memory

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ var number_one = null;
 var number_two = null;
 var operator = null;
 var result = null;
+var memory = null;
 
 // Transform array into number, length is limited by numberOfDigits
 function arrToNum (array) {
@@ -17,12 +18,15 @@ function arrToNum (array) {
 // Transorm number into array, length is limited by 8 numberOfDigits
 function numToArr (number) {
     let array = number.toString().split("");
-    if (array.length <= numberOfDigits) {
+    if (array.length <= numberOfDigits) { 
         return array;
     };
     let array1 = [];
     for (let i = 0; i < numberOfDigits; i++) {
         array1.push(array[i]);
+    };
+    if (array1[array1.length - 1] === ".") {    // delete "." in the end
+        array1.pop();
     };
     return array1;
 };
@@ -46,5 +50,10 @@ function calculate (num1, num2, oper) {
     };
 };
 
-
+// Display number
+function displayNumber (number) {
+    let string = numToArr(number).join(""); // use function to limit digits number
+    let display = document.getElementById("display");
+    display.textContent = string;
+}
 
