c36398913fd73320cbc240dc4d0a688b8abc970c
Dimm005 <dimm005@gmail.com>
2023-11-22 19:08:50 +0800

    Changes in HTML and script
    HTML:
    divide display into two parts - number_display and sign-display
    (to show sign of operator) with <span>. The second argument (or result
    of previous operation) and the operator should be visible
    simultaneously.
    
    Script:
    According to changes in HTML, change number output to
    numberDisplay instead display element.
    
    Fix roundNumber function. Also now the maximal length of positive and
    negative numbers is the same (because it can be changed by user).
    
    Fix onclickDigitEvent function work with number length and change .pop
    to .shift when deleting zero in the begining of the number (it works,
    but it is ridiculous :-))

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 // Global constants and variables
-const numberOfDigits = 8; 
-var display = document.getElementById("display");
+const numberOfDigits = 10; 
+const numberDisplay = document.getElementById("number_display");
 var numberOne = [];
 var numberTwo = [];
 var numberInputArray = [];
 var numberMarker = 1; // shows what number we are working with, One or Two
 var operator = "";
-var result = null;
+// var result = null;
 var memory = null;
 
 // Add event listeners to digit buttons
@@ -20,6 +20,10 @@ const operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener("click", onclickOperatorEvent);
 });
+
+// Add event listener to equal button
+const equalButton = document.getElementById("equal_button");
+equalButton.addEventListener("click", onclickEqualEvent);
 
 // Transform array into number
 function arrToNum (array) {
@@ -66,30 +70,26 @@ function displayNumber (number) {
     } else {
         string = number.toString(); 
     };
-    display.textContent = string;
+    numberDisplay.textContent = string;
 }; 
 
 // Round number to max number of digits (or return ERROR)
 function roundNumber (number) {
-    let maxDisplayedNumber = ""; // maximum number we can display
+    let maxDisplayedNumber = "9"; // maximum number we can display
     let signMarker = 1; // is a number positive or negative
-    let jMax = 0; // for negative numbers cycle should be 1 step shorter
+    let jMax = numberOfDigits - 2;
     if (number < 0) {
         signMarker = -1;
         number *= signMarker; // make a number positive
-        jMax = numberOfDigits - 1;
-    } else {
-        jMax = numberOfDigits;
     };
-
     for (let j = 0; j < jMax; j++) {
         maxDisplayedNumber += "9";
     };
     maxDisplayedNumber = parseInt(maxDisplayedNumber);
-
     let array = number.toString().split("");
     let i = array.findIndex((element) => element == ".");
-    let tens = Math.pow(10, jMax - i - 1);
+    let tens = Math.pow(10, jMax - i);
+
     number = number * tens;
     number = Math.round(number) / tens;
     if (number > maxDisplayedNumber) {
@@ -102,25 +102,25 @@ function roundNumber (number) {
 function onclickDigitEvent () {
     switch (numberMarker) {
         case 1:
-            if (numberOne.length < numberOfDigits) {
+            if (numberOne.length < numberOfDigits - 1) {
                 if (numberOne[0] == "0") {
-                    numberOne.pop();
+                    numberOne.shift();
                 };
                 numberOne.push(this.id);
             };
             displayNumber(arrToNum(numberOne));
             break;
         case 2:
-            if (numberTwo.length < numberOfDigits) {
+            if (numberTwo.length < numberOfDigits - 1) {
                 if (numberTwo[0] == "0") {
-                    numberTwo.pop();
+                    numberTwo.shift();
                 };
                 numberTwo.push(this.id)
             };
             displayNumber(arrToNum(numberTwo));
             break;
     };
-
+    return;
 };
 
 // Operator buttons event function
@@ -132,10 +132,33 @@ function onclickOperatorEvent () {
         let number = arrToNum(numberOne);
         number = calculate(number, 0, "*=");
         displayNumber(number);
-        numberOne = [];
+        numberOne = numToArr(number);
+        numberTwo = [];
+        operator = "";
         return;
     };
+    numberTwo = [];
     numberMarker = 2;
     operator = this.id;
     display.textContent = operator;
 };
+
+// Equal button event function
+function onclickEqualEvent () {
+    if (numberOne.length == 0 || numberTwo.length == 0) {
+        operator = "";
+        return;
+    };
+    let num1 = arrToNum(numberOne);
+    let num2 = arrToNum(numberTwo);
+    if (operator == "") {
+        return;
+    };
+    let resultNumber = calculate(num1, num2, operator);
+    displayNumber(resultNumber);
+    numberOne = numToArr(resultNumber);
+    numberTwo = [];
+    operator = "";
+    numberMarker = 1;
+    return;
+}
